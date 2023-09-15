80cb33c2118b2cb31ca1bf4ee249fe9ed3fd10ed
Hasibul Hasan Rupok <86670176+HasibulRupok@users.noreply.github.com>
2023-09-15 22:37:23 +0600

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,26 @@
+// Function to toggle between dark and light modes
+function toggleMode() {
+    console.log('clicked');
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+
+    // Get the current mode
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    // Update the CSS variables based on the mode
+    document.documentElement.style.setProperty('--fontColor', isDarkMode ? '#6c757d' : 'black');
+    document.documentElement.style.setProperty('--BGcolor', isDarkMode ? '#17191a' : 'white');
+
+    // update nav 
+    document.documentElement.style.setProperty('--navBG', isDarkMode ? '#242627' : '#eeeeee');
+
+    // update the toggle icon 
+    const toggleButton = document.getElementById('toggle-mode');
+    if(isDarkMode) toggleButton.innerHTML = "<i class='fa-solid fa-sun' style='color: #ffc800;'></i>";
+    else toggleButton.innerHTML = "<i class='fa-solid fa-moon'></i>";
+    
+}
+const modeToggleBtn = document.getElementById('toggle-mode');
+modeToggleBtn.addEventListener('click', toggleMode);
+
